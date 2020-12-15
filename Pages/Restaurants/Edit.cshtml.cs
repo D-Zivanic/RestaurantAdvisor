14fa73c5b78fae2f71c25c1b71c860136ddc4e44
David Zivanic <davidzivanic02@gmail.com>
2020-12-15 15:24:43 +0000

    Created form for editing restaurant

--- Pages/Restaurants/Edit.cshtml.cs
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RestaurantAdvisor.Core;
 using RestaurantAdvisor.Data;
 
@@ -12,18 +13,22 @@ namespace RestaurantAdvisor.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData _restaurantData;
+        private readonly IHtmlHelper _htmlHelp;
 
-        public Restaurant restaurant {get; set; }
+        public Restaurant restaurant {get; set;}
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelp)
         {
             _restaurantData = restaurantData;
+            _htmlHelp = htmlHelp;
         }
 
         public void OnGet(int id)
         {
+            Cuisines = _htmlHelp.GetEnumSelectList<CuisineType>();
             restaurant = _restaurantData.GetSingle(id);
-
+            
         }
     }
 }
