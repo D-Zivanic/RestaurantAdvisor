0102f630a70eacd3d7d81cfd5bf45ee1f846d316
David Zivanic <davidzivanic02@gmail.com>
2020-12-22 12:51:33 +0000

    .

--- Pages/Restaurants/Edit.cshtml.cs
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -15,6 +15,7 @@ namespace RestaurantAdvisor.Pages.Restaurants
         private readonly IRestaurantData _restaurantData;
         private readonly IHtmlHelper _htmlHelp;
 
+        [BindProperty]
         public Restaurant restaurant {get; set;}
         public IEnumerable<SelectListItem> Cuisines { get; set; }
 
@@ -24,11 +25,46 @@ namespace RestaurantAdvisor.Pages.Restaurants
             _htmlHelp = htmlHelp;
         }
 
-        public void OnGet(int id)
+        public IActionResult OnGet(int? id)
         {
             Cuisines = _htmlHelp.GetEnumSelectList<CuisineType>();
-            restaurant = _restaurantData.GetSingle(id);
+            if (id.HasValue)
+            {
+                restaurant = _restaurantData.GetSingle(id.Value);
+            }
+            else
+            {
+                restaurant = new Restaurant();
+            }
             
+            if (restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Cuisines = _htmlHelp.GetEnumSelectList<CuisineType>();
+                return Page();
+            }
+
+            if (restaurant.Id > 0)
+            {
+                _restaurantData.Update(restaurant);
+            }
+            else
+            {
+                _restaurantData.Create(restaurant);
+            }
+
+            _restaurantData.Save();
+            return RedirectToPage("./Detail", new { restaurant.Id });
         }
     }
 }
